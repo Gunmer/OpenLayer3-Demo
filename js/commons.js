198ed2f30646f7ec27bc24102438f8e6a89670c0
cristiam <cristiam.sosa@innocv.com>
2015-05-07 16:19:32 +0200

    Clustering

--- js/commons.js
+++ js/commons.js
@@ -1,25 +1,8 @@
-//Global variables
-var features = []
-
-function addMarker(order, coordinate, img, scale){
-	var feature = new ol.Feature(new ol.geom.Point(coordinate));
-	feature.setStyle(new ol.style.Style({
-	  image: new ol.style.Icon(({
-	    opacity: 1,
-	    src: img,
-	    scale: scale
-	  }))
-	}));
-
-	features[order] = feature;
-}
-
-function getMarkerSource(){
-	return new ol.source.Vector({
-	  features: features
-	});
-}
 
 function changePointToCoordinate(point){
-	return ol.proj.transform([point[0], point[1]], 'EPSG:3857', 'EPSG:4326')
+	return ol.proj.transform([point[0], point[1]], 'EPSG:3857', 'EPSG:4326');
+}
+
+function changeCoordinateToPoint(coordinates){
+	return ol.proj.transform([coordinates[0], coordinates[1]], 'EPSG:4326', 'EPSG:3857');
 }
